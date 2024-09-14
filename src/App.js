65e4bb4f083583d68801f0ff14787ec6a95eafcb
Elsabordeaux <snowpaws85@gmail.com>
2024-09-14 19:55:30 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <p>
-          This project was coded by{" "}
-          <a href="https://github.com/Elsabordeaux">Alana Jackson</a> and is
-          open-sourced on{" "}
-          <a href="https://github.com/Elsabordeaux/alanas-react-weather-app">
-            Github
-          </a>{" "}
-          and hosted on{" "}
-          <a href="https://alanas-react-weather-app.netlify.app/">Netlify</a>
-        </p>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a href="https://github.com/Elsabordeaux">Alana Jackson</a> and is
+            open-sourced on{" "}
+            <a href="https://github.com/Elsabordeaux/alanas-react-weather-app">
+              Github
+            </a>{" "}
+            and hosted on{" "}
+            <a href="https://alanas-react-weather-app.netlify.app/">Netlify</a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
